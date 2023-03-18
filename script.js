79fde6c60d95d917eed2ba169657a90ef51c76a9
Thybow <tmilville.pro@gmail.com>
2023-03-18 15:32:14 +0100

    second part

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 let button = document.querySelector(".button")
 
 button.addEventListener("click", ()=>{
-    button.classList.add("active");
+    button.classList.add("active")
+
+    setTimeout(()=>{
+        button.classList.remove("active"); // remove active class
+        document.querySelector("i").classList.replace("bx-cloud-download", "bx-check-circle")
+        document.querySelector(".button-text").innerText = "Completed"
+    },6000) // 1s = 1000ms
 })
